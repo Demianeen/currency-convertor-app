aaa55e8b4d157829e624aeb2af072a9284f3074e
Demian <dnetlyukh@gmail.com>
2022-08-09 22:19:47 +0300

    Remove unused inmports

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,6 +1,5 @@
 export { default as StyledNumberFormat } from './StyledNumberFormat/StyledNumberFormat'
 export { default as StyledSelect } from './StyledSelect/StyledSelect'
-export { default as StyledAutocomplete } from './StyledAutocomplete/StyledAutocomplete'
 export { default as P } from './P/P'
 export { default as Htag } from './Htag/Htag'
 export { default as ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher'
